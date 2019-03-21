842e3d77d9bf300f835571fd9e0b0444c885f1d6
Zach Schwartz <zischwartz@gmail.com>
2019-03-21 11:00:44 -0400

    sort on load, closes #1

--- src/index.js
+++ src/index.js
@@ -8,7 +8,15 @@ let data = null;
 const settings = {
   community_scan_data_table: el => {
     // console.log(data);
-    RenderDataTable(el, data["Sheet1"]);
+    let sheet_data = data["Sheet1"];
+    sheet_data = sheet_data.sort((a, b) => {
+      //prettier-ignore
+      if(a["Name of Program"] < b["Name of Program"]) { return -1; }
+      //prettier-ignore
+      if(a["Name of Program"] > b["Name of Program"]) { return 1; }
+      return 0;
+    });
+    RenderDataTable(el, sheet_data);
     // use data
   }
 };
